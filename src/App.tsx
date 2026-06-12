9f6082829ac3e4ac1f17f64fb76838b306ea96e5
[dyad] <meyertiffertgbr@gmail.com>
2026-06-12 09:54:30 +0200

    [dyad] Scroll-Position beim Seitenwechsel zurücksetzen - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Index from './pages/Index';
 import Waitlist from './pages/Waitlist';
 import Success from './pages/Success';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/waitlist" element={<Waitlist />} />
